e1efc9b48bd09439aeaf962daf62af808a2cf94d
PMKS RATHNAYAKA <123926770+KanchanaSaranga@users.noreply.github.com>
2023-09-06 22:01:17 +0530

    Changes done

---
+++ Common/header.php
@@ -0,0 +1,41 @@
+
+
+<header>
+<section class>
+<nav class="navbar navbar-expand-lg navbar-toggler bg-dark py-4 text-white">
+    <div class="navbar__container">
+        <div class="navbar__logo" style="width: 50px; height: 50px;">
+            <img src="logo.jpg" alt="Kasthuri Mobile solutions" max-width="20" max-height="50">
+        </div>
+        
+        <div class="navbar__toggle" id="mobile-menu">
+            <span class="bar"></span>
+            <span class="bar"></span>
+            <span class="bar"></span>
+            <span class="bar"></span>
+        </div>
+        <ul class="navbar__menu">
+            <li class="navbar__item">
+               <a href="#home" class="navbar__links" >Home </a> 
+            </li>
+            <li class="navbar__item">
+                <a href="about.php" class="navbar__links">About Us </a> 
+            </li>
+            <li class="navbar__item">
+                <a href="services.html" class="navbar__links">Services</a> 
+            </li>
+            <li class="navbar__item">
+                <a href="contact us.html" class="navbar__links">Contact Us </a> 
+            </li>
+            <li class="navbar__btn">
+                <a href="login.php" class="admin">Admin Login </a> 
+            </li>
+        </ul>
+    </div>
+</nav>
+</section>
+
+<script src="app.js"></script>
+</header>
+
+ 
--- admin.php
+++ admin.php
@@ -5,6 +5,7 @@
     </head>
 
     <body>
+    <?php include_once("./Common/header.php") ?>
         <h1>You are a Admin</h1>
     </body>
 </html>
