b09cd5a4d015a67ac8a831eee0dcb6a3911cfbc6
PMKS RATHNAYAKA <123926770+KanchanaSaranga@users.noreply.github.com>
2023-09-06 23:08:58 +0530

    name changed

--- login.php
+++ login.php
@@ -1,66 +1,72 @@
-<?php
+<?php session_start();
 
-$host="localhost";
-$user="root";
-$password="";
-$db="user";
+$host = "localhost";
+$user = "root";
+$password = "";
+$db = "mobileShopDb";
 
-$data=mysqli_connect($host,$user,$password,$db);
-if($data===false)
-{
+$data = mysqli_connect($host, $user, $password, $db);
+if ($data === false) {
     die("connection error");
 }
 
-if($_SERVER["REQUEST_METHOD"]=="POST")
-{
-    $username = $_POST["username"];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = $_POST["username"];
     $password = $_POST["password"];
 
-    if (!empty($username) && !empty($password)) {
-        $sql="select * from login where username= '".$username."' AND password='".$password."' ";
 
-        $result=mysqli_query($data,$sql);
-        $row=mysqli_fetch_array($result);
+    if (!empty($email) && !empty($password)) {
+        $sql = "select * from systemuser where email= '" . $email . "' AND password='" . $password . "' ";
+     
+        $result = mysqli_query($data, $sql);
+        print_r($result->num_rows == 1);
 
-        if($row["usertype"]=="user")
-        {
-           header("location:tech.php");
-        }   
-        
-        elseif($row["usertype"]=="admin")
-        { 
-            header("location:admin.php");
-        }    
-        else
-        {
-            echo "username or passworrd incorrect";
+        if ($result->num_rows == 1) {
+
+            $row = mysqli_fetch_array($result);
+
+            print_r($row);
+            $_SESSION["systemUserID"] = $row["empNo"];
+
+            $sql = "select * from location where locid = " . $row["locid"];
+            $result = mysqli_query($data, $sql);
+            $row = mysqli_fetch_array($result);
+            $_SESSION["locationId"] = $row["locid"];
+            $_SESSION["locationName"] = $row["name"];
+
+
+            header("Location: index.php"); // Redirect to a welcome page or dashboard
+        } else {
+            // Invalid login, display an error message
+            echo "Invalid username or password.";
         }
     } else {
         echo "Please enter username and password";
     }
-          
 }
 
 ?>
-        
+
 <!DOCTYPE html>
 <html>
 
 <head>
-	<title>Transparent Login Form</title>
-	<link rel="stylesheet" type="text/css" href="login.css">
+    <title>Transparent Login</title>
+    <link rel="stylesheet" type="text/css" href="login.css">
 </head>
+
 <body>
-	<div class="login-box">
-		<h1>Login</h1>
-		<form action="#" method="POST">
-			<p>Username</p>
-			<input type="text" name="username" required>
-			<p>Password</p>
-			<input type="password" name="password" required>
-			<input type="submit" name="" value="Login">
-			<a href="#">Forgot Password</a>
-		</form>
-	</div>
+    <div class="login-box">
+        <h1>Login</h1>
+        <form action="" method="POST">
+            <p>Username</p>
+            <input type="text" name="username" required>
+            <p>Password</p>
+            <input type="password" name="password" required>
+            <input type="submit" name="" value="Login">
+            <a href="#">Forgot Password</a>
+        </form>
+    </div>
 </body>
+
 </html>
