6f9b90e899a62ba07c07ccf034d52af560ca33b7
UE SINGHAPURA <140133331+Eharagithub@users.noreply.github.com>
2023-09-10 19:47:01 +0530

    Add services page

--- Common/header.php
+++ Common/header.php
@@ -5,7 +5,7 @@
 <nav class="navbar navbar-expand-lg navbar-toggler bg-dark py-4 text-white">
     <div class="navbar__container">
         <div class="navbar__logo" style="width: 50px; height: 50px;">
-            <img src="logo.jpg" alt="Kasthuri Mobile solutions" max-width="20" max-height="50">
+            <img src="new.jpg" alt="Kasthuri Mobile solutions" max-width="20" max-height="50">
         </div>
         
         <div class="navbar__toggle" id="mobile-menu">
@@ -16,16 +16,16 @@
         </div>
         <ul class="navbar__menu">
             <li class="navbar__item">
-               <a href="#home" class="navbar__links" >Home </a> 
+               <a href="index.php" class="navbar__links" >Home </a> 
             </li>
             <li class="navbar__item">
                 <a href="about.php" class="navbar__links">About Us </a> 
             </li>
             <li class="navbar__item">
-                <a href="services.html" class="navbar__links">Services</a> 
+                <a href="services.php" class="navbar__links">Services</a> 
             </li>
             <li class="navbar__item">
-                <a href="contact us.html" class="navbar__links">Contact Us </a> 
+                <a href="contact.php" class="navbar__links">Contact Us </a> 
             </li>
             <li class="navbar__btn">
                 <a href="login.php" class="admin">Admin Login </a> 
--- about.php
+++ about.php
@@ -9,49 +9,17 @@
     </head>
     <body>
         <header>
-            <section class>
-            <nav class="navbar navbar-expand-lg navbar-toggler bg-dark py-4 text-white">
-                <div class="navbar__container">
-                    <div class="navbar__logo"><img src="logo.jpg" alt="Kasthuri Mobile solutions" width="100" height="200"></div>
-                    
-                    <div class="navbar__toggle" id="mobile-menu">
-                        <span class="bar"></span>
-                        <span class="bar"></span>
-                        <span class="bar"></span>
-                        <span class="bar"></span>
-                    </div>
-                    <ul class="navbar__menu">
-                        <li class="navbar__item">
-                           <a href="index.php" class="navbar__links" >Home </a> 
-                        </li>
-                        <li class="navbar__item">
-                            <a href="#about" class="navbar__links">About Us </a> 
-                        </li>
-                        <li class="navbar__item">
-                            <a href="services.html" class="navbar__links">Services</a> 
-                        </li>
-                        <li class="navbar__item">
-                            <a href="contact us.html" class="navbar__links">Contact Us </a> 
-                        </li>
-                        <li class="navbar__btn">
-                            <a href="login.php" class="admin">Admin Login </a> 
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-            <body>
-                <div class="transparent-panel">
-                  <h2>About Us</h2>
-                  <p>iPhone technologies founded in 2005, is Srilanka’s leading solutions provider for all Apple devices. Though many believe the company’s name to have been inspired by the Apple product, iPhone technologies had already commenced their journey 2 years before the launch of the first iPhone.<br><br>Having established a reputation for reliability and quality, customer satisfaction is our highest priority, and we guarantee expert solutions to all your Apple product problems.</p>
-                </div>
-              </body>
-        </section>
+        <?php include_once("./Common/header.php") ?> </header>
+            
+            <section class="transparent-panel">
+                 <h2>About Us</h2>
+                 <p>iPhone technologies founded in 2005, is Srilanka’s leading solutions provider for all Apple devices. Though many believe the company’s name to have been inspired by the Apple product, iPhone technologies had already commenced their journey 2 years before the launch of the first iPhone.<br><br>Having established a reputation for reliability and quality, customer satisfaction is our highest priority, and we guarantee expert solutions to all your Apple product problems.</p>
+               </div>
+    
+            </section>
+       
 
-        </header>
+    </body>
 
-        
-
-        </body>
-
-        </html>
-        
+ </html>
+            
