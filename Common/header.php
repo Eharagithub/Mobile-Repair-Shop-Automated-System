bf93278996b6bb552cb6de6443b1bb8fd8256d7f
PMKS RATHNAYAKA <123926770+KanchanaSaranga@users.noreply.github.com>
2023-10-06 17:16:44 +0530

    logo

--- Common/header.php
+++ Common/header.php
@@ -1,45 +1,44 @@
 <header>
+    <section class>
 
-<section class>
+        <nav class="navbar navbar-expand-lg navbar-toggler bg-dark py-4 text-white">
+            <div class="navbar__container">
+                <!-- Move the logo inside the nav element -->
+                <a href="index.php">
+                <div class="navbar__logo" style="width: 200px; height: 100px; display: flex; justify-content: center; align-items: center; margin-top: 0px; margin-left: -100px;">
+                    <img src="new.jpg" alt="Kasthuri Mobile solutions" style="max-width: 100%; max-height: 100%;">
+                </div>
 
-<nav class="navbar navbar-expand-lg navbar-toggler bg-dark py-4 text-white">
-    <div class="navbar__container">
-
-        <div class="navbar__logo" style="width: 50px; height: 100px; display: flex; justify-content: center; align-items: center;">
-            <img src="new.jpg" alt="Kasthuri Mobile solutions" style="max-width: 100%; max-height: 100%;">
-
-        </div>
-        
-        <div class="navbar__toggle" id="mobile-menu">
-            <span class="bar"></span>
-            <span class="bar"></span>
-            <span class="bar"></span>
-            <span class="bar"></span>
-        </div>
-        <ul class="navbar__menu">
-            <li class="navbar__item">
-               <a href="index.php" class="navbar__links" >Home </a> 
-            </li>
-            <li class="navbar__item">
-                <a href="about.php" class="navbar__links">About Us </a> 
-            </li>
-            <li class="navbar__item">
-                <a href="Servicess.php" class="navbar__links">Services</a> 
-            </li>
-            <li class="navbar__item">
-                <a href="contact.php" class="navbar__links">Contact Us </a> 
-            </li>
-            <li class="navbar__btn">
-                <a href="" class="admin">Login </a> 
-                <ul class ="dropdown">
-                    <li><a href="login.php">Admin Login</a></li>
-                    <li><a href="http://localhost/Mobile-Repair-Shop/suboffice/index.php">Sub-Office Login</a></li>
-                    <li><a href="http://localhost/Mobile-Repair-Shop/technician/index.php">Technician Login</a></li>
+                <div class="navbar__toggle" id="mobile-menu">
+                    <span class="bar"></span>
+                    <span class="bar"></span>
+                    <span class="bar"></span>
+                    <span class="bar"></span>
+                </div>
+                <ul class="navbar__menu">
+                    <li class="navbar__item">
+                        <a href="index.php" class="navbar__links" >Home </a> 
+                    </li>
+                    <li class="navbar__item">
+                        <a href="about.php" class="navbar__links">About Us </a> 
+                    </li>
+                    <li class="navbar__item">
+                        <a href="Servicess.php" class="navbar__links">Services</a> 
+                    </li>
+                    <li class="navbar__item">
+                        <a href="contact.php" class="navbar__links">Contact Us </a> 
+                    </li>
+                    <li class="navbar__btn">
+                        <a href="" class="admin">Login </a> 
+                        <ul class ="dropdown">
+                            <li><a href="login.php">Admin Login</a></li>
+                            <li><a href="http://localhost/Mobile-Repair-Shop/suboffice/index.php">Sub-Office Login</a></li>
+                            <li><a href="http://localhost/Mobile-Repair-Shop/technician/index.php">Technician Login</a></li>
+                        </ul>
+                    </li>
                 </ul>
-            </li>
-        </ul>
-    </div>
-</nav>
-</section>
-<script src="app.js"></script>
+            </div>
+        </nav>
+    </section>
+    <script src="app.js"></script>
 </header>
