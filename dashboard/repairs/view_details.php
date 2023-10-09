b3a3d03bd7d07ace5d349c7f47132cddc9cc0336
RAKK RANASINGHE <140103169+Kebuni22@users.noreply.github.com>
2023-10-09 19:04:17 +0530

    Script code

--- dashboard/repairs/view_details.php
+++ dashboard/repairs/view_details.php
@@ -1,32 +1,3 @@
 
 
 
-<script>
-    $(function(){
-        $('#delete_data').click(function(){
-			_conf("Are you sure to delete <b><?= $code ?>\'s</b> from repair permanently?","delete_repair",['<?= $id ?>'])
-		})
-    })
-    function delete_repair($id){
-		start_loader();
-		$.ajax({
-			url:_base_url_+"classes/Master.php?f=delete_repair",
-			method:"POST",
-			data:{id: $id},
-			dataType:"json",
-			error:err=>{
-				console.log(err)
-				alert_toast("An error occured.",'error');
-				end_loader();
-			},
-			success:function(resp){
-				if(typeof resp== 'object' && resp.status == 'success'){
-					location.replace('./?page=repairs');
-				}else{
-					alert_toast("An error occured.",'error');
-					end_loader();
-				}
-			}
-		})
-	}
-</script>
