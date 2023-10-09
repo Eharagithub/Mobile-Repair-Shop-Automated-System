0e1d1bdd4a9525104c576e820b71d9f07666db6c
RAKK RANASINGHE <140103169+Kebuni22@users.noreply.github.com>
2023-10-09 18:53:02 +0530

    JS code

--- dashboard/repairs/index.php
+++ dashboard/repairs/index.php
@@ -21,38 +21,3 @@
 		</div>
 	</div>
 </div>
-<script>
-	$(document).ready(function(){
-		$('.delete_data').click(function(){
-			_conf("Are you sure to delete this repair permanently?","delete_repair",[$(this).attr('data-id')])
-		})
-		$('.table td,.table th').addClass('py-1 px-2 align-middle')
-		$('.table').dataTable({
-            columnDefs: [
-                { orderable: false, targets: 5 }
-            ],
-        });
-	})
-	function delete_repair($id){
-		start_loader();
-		$.ajax({
-			url:_base_url_+"classes/Master.php?f=delete_repair",
-			method:"POST",
-			data:{id: $id},
-			dataType:"json",
-			error:err=>{
-				console.log(err)
-				alert_toast("An error occured.",'error');
-				end_loader();
-			},
-			success:function(resp){
-				if(typeof resp== 'object' && resp.status == 'success'){
-					location.reload();
-				}else{
-					alert_toast("An error occured.",'error');
-					end_loader();
-				}
-			}
-		})
-	}
-</script>
