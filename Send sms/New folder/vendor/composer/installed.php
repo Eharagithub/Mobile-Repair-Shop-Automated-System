59c1a0a35dd6670e04db90afcfe74430f719262e
PMKS RATHNAYAKA <123926770+KanchanaSaranga@users.noreply.github.com>
2023-10-18 08:36:50 +0530

    update

---
+++ Send sms/New folder/vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '36dedc92bed8dd9cd15354690b4c78b0fb107e60',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '36dedc92bed8dd9cd15354690b4c78b0fb107e60',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'notifylk/notify-php' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '7fbd8cff4a089a6a21c3802638241ded1ff18c89',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../notifylk/notify-php',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+    ),
+);
